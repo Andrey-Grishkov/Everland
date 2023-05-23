b920fea070a4f1ba54e556eff5f0a99f457e954c
Yakov Samsonov <yasamsonov@gmail.com>
2023-05-23 17:08:31 +0300

    Refactor: new sliders

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,15 +1,6 @@
-const projectsContainer = document.querySelector('.projects__container');
-const sliderContainer = document.querySelector('.cards-container_type_projects');
-const sliderInfiniteArrowLeft = document.querySelector('.projects__arrow_left');
-const sliderInfiniteArrowRight = document.querySelector('.projects__arrow_right');
-const visibleCardSlider = document.querySelector('.card_visible');
-const firstSliderContainer = document.querySelector('.cards-container_type_description');
-const firstSliderArrowLeft = document.querySelector('.description__slider-button_type_left');
-const firstSliderArrowRight = document.querySelector('.description__slider-button_type_right');
-const sliderCounter = document.querySelector('.description__span');
 const burgerPopupArrows = document.querySelectorAll('.burger-popup__button');
 const burgerMenu = document.querySelector('.header__menu');
 const burgerMenuIcon = document.querySelector('.header__menu-image');
 const burgerPopup = document.querySelector('.burger-popup');
 
-export {projectsContainer, sliderContainer, sliderInfiniteArrowLeft, sliderInfiniteArrowRight, visibleCardSlider, firstSliderContainer, firstSliderArrowLeft, firstSliderArrowRight, sliderCounter, burgerPopupArrows, burgerMenu, burgerMenuIcon, burgerPopup};
+export {burgerPopupArrows, burgerMenu, burgerMenuIcon, burgerPopup};
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,50 +1,192 @@
-import {sliderCounter} from "./constants.js";
-
-function disableOneButton(leftArrow, rightArrow, count) {
-  if (count === 0) {
-    leftArrow.disabled = true;
-    leftArrow.classList.add('button_disabled');
-  } else {
-    leftArrow.classList.remove('button_disabled');
+class Slider {
+  static transitionStyle = "translate 0.5s ease-in-out";
+  static transitionTimeout = 500;
+  constructor({ sliderContainer, controlContainer, labelContainer }) {
+    this._sliderContainer = sliderContainer;
+    this._controlContainer = controlContainer;
+    this._labelContainer = labelContainer;
+    this._rightButton = this._controlContainer.querySelector(
+      ".slider__button[name=right]"
+    );
+    this._leftButton = this._controlContainer.querySelector(
+      ".slider__button[name=left]"
+    );
+    this._currentItem = 1;
+    this._totalItem = sliderContainer.children.length;
   }
-  if (count === 5) {
-    rightArrow.disabled = true;
-    rightArrow.classList.add('button_disabled');
-  } else {
-    rightArrow.classList.remove('button_disabled');
+
+  _baseInitiate() {
+    this._setListeners();
+    this._calculateLabel();
+    this._sliderContainer.style.setProperty("--slides", this._totalItem);
+  }
+
+  initiate() {
+    this._baseInitiate();
+    this._checkControls();
+  }
+
+  _calculateLabel() {
+    if (this._labelContainer) {
+      this._labelContainer.textContent = `${this._currentItem}/${this._totalItem}`;
+    }
+  }
+
+  _checkControls() {
+    if (this._totalItem === 1) {
+      this._disableAllControls();
+    } else if (this._currentItem === 1) {
+      this._disableControl(this._leftButton);
+      this._enableControl(this._rightButton);
+    } else if (this._currentItem === this._totalItem) {
+      this._disableControl(this._rightButton);
+      this._enableControl(this._leftButton);
+    } else {
+      this._enableAllControls();
+    }
+  }
+
+  _setListeners() {
+    this._leftButton.addEventListener("click", () => {
+      this._leftCLick();
+    });
+
+    this._rightButton.addEventListener("click", () => {
+      this._rightClick();
+    });
+  }
+
+  _getStep() {
+    return 100 / this._totalItem;
+  }
+
+  _getShift(position) {
+    const shift = position ? position : this._currentItem;
+    return -1 * (shift - 1) * this._getStep();
+  }
+
+  _animatedMove(postion) {
+    this._sliderContainer.style.transition = this.constructor.transitionStyle;
+    this._sliderContainer.style.translate = `${this._getShift(postion)}%`;
+  }
+
+  _instantMove(position) {
+    this._sliderContainer.style.removeProperty("transition");
+    this._sliderContainer.style.translate = `${this._getShift(position)}%`;
+  }
+
+  _drawSlider() {
+    this._disableAllControls();
+    this._animatedMove();
+
+    setTimeout(() => {
+      this._enableAllControls();
+      this._calculateLabel();
+      this._checkControls();
+    }, this.constructor.transitionTimeout);
+  }
+
+  _leftCLick() {
+    this._currentItem -= 1;
+    this._drawSlider();
+  }
+
+  _rightClick() {
+    this._currentItem += 1;
+    this._drawSlider();
+  }
+
+  _disableAllControls() {
+    this._disableControl(this._leftButton);
+    this._disableControl(this._rightButton);
+  }
+
+  _enableAllControls() {
+    this._enableControl(this._leftButton);
+    this._enableControl(this._rightButton);
+  }
+
+  _disableControl(control) {
+    control.disabled = true;
+    control.classList.add('button_disabled');
+  }
+
+  _enableControl(control) {
+    control.disabled = false;
+    control.classList.remove('button_disabled');
   }
 }
 
-function disableButton(leftArrow, rightArrow, boolean) {
-  leftArrow.disabled = boolean;
-  rightArrow.disabled = boolean;
+class InfiniteSlider extends Slider {
+  constructor(params) {
+    super(params);
+  }
+
+  initiate() {
+    super._baseInitiate();
+    this._prepareInfiniteSlider();
+  }
+
+  _prepareInfiniteSlider() {
+    const item = this._sliderContainer
+      .querySelector(".slider__item:last-of-type")
+      .cloneNode(true);
+    this._sliderContainer.querySelector(".slider__item:last-of-type").remove();
+    this._sliderContainer.prepend(item);
+    this._instantMove(2);
+  }
+
+  _moveLeft() {
+    const item = this._sliderContainer
+      .querySelector(".slider__item:last-of-type")
+      .cloneNode(true);
+    this._disableAllControls();
+    this._animatedMove(1);
+    setTimeout(() => {
+      this._sliderContainer
+        .querySelector(".slider__item:last-of-type")
+        .remove();
+      this._instantMove(2);
+      this._sliderContainer.prepend(item);
+      this._enableAllControls();
+    }, super.constructor.transitionTimeout);
+  }
+
+  _moveRight() {
+    const item = this._sliderContainer
+      .querySelector(".slider__item:first-of-type")
+      .cloneNode(true);
+    this._disableAllControls();
+    this._animatedMove(3);
+    setTimeout(() => {
+      this._sliderContainer
+        .querySelector(".slider__item:first-of-type")
+        .remove();
+      this._instantMove(2);
+      this._sliderContainer.append(item);
+      this._enableAllControls();
+    }, super.constructor.transitionTimeout);
+  }
+
+  _leftCLick() {
+    if (this._currentItem === 1) {
+      this._currentItem = this._totalItem;
+    } else {
+      this._currentItem -= 1;
+    }
+    this._moveLeft();
+    this._calculateLabel();
+  }
+
+  _rightClick() {
+    if (this._currentItem === this._totalItem) {
+      this._currentItem = 1;
+    } else {
+      this._currentItem += 1;
+    }
+    this._moveRight();
+    this._calculateLabel();
+  }
 }
 
-function countSlides(count) {
-  sliderCounter.textContent = `${count+1}/6`;
-}
-
-// Функция описывает сдвиг контейнера по процентам.
-function moveSlides(element, persent) {
-  element.style.translate = `${persent}%`;
-}
-
-function moveLeft(element, persent, count, leftArrow, rightArrow) {
-  moveSlides(element, persent);
-  disableButton(leftArrow, rightArrow, true);
-  setTimeout(() => {
-    disableButton(leftArrow, rightArrow, false);
-    disableOneButton(leftArrow, rightArrow, count);
-  }, 500);
-}
-
-function moveRight(element, persent, count, leftArrow, rightArrow) {
-  moveSlides(element, persent);
-  disableButton(leftArrow, rightArrow, true);
-  setTimeout(() => {
-    disableButton(leftArrow, rightArrow, false);
-    disableOneButton(leftArrow, rightArrow, count);
-  }, 500);
-}
-
-export {moveLeft, moveRight, countSlides};
+export { Slider, InfiniteSlider };
